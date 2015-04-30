c569be9fe68e62862d804bef8e9570107d5bbb4d
Zhestokus <v.ezugbaia@gmail.com>
2015-04-30 16:49:16 +0400

    dotNet-ASP.NET.TwoWayModel

--- ASP.NET.TwoWayModel/UIBases/PageModelBase.cs
+++ ASP.NET.TwoWayModel/UIBases/PageModelBase.cs
@@ -21,22 +21,30 @@ namespace ASP.NET.TwoWayModel.UIBases
 
         public virtual Object GetModel(Type type)
         {
-            return ModelProcessor.GetModel(type);
+            var model = ModelProcessor.GetModel(type);
+            OnGetModel(model, type);
+
+            return model;
         }
 
         public virtual Object GetModel(Type type, Func<Control, Type, Object> valueGetter)
         {
-            return ModelProcessor.GetModel(type, valueGetter);
+            var model = ModelProcessor.GetModel(type, valueGetter);
+            OnGetModel(model, type);
+
+            return model;
         }
 
         public virtual void FillModel(Object model, Type type)
         {
             ModelProcessor.FillModel(model, type);
+            OnFillModel(model, type);
         }
 
         public virtual void FillModel(Object model, Type type, Func<Control, Type, Object> valueGetter)
         {
             ModelProcessor.FillModel(model, type, valueGetter);
+            OnFillModel(model, type);
         }
 
         public virtual void SetModel(Object model, Type type)
@@ -57,22 +65,30 @@ namespace ASP.NET.TwoWayModel.UIBases
 
         public virtual TModel GetModel<TModel>() where TModel : class, new()
         {
-            return ModelProcessor.GetModel<TModel>();
+            var model = ModelProcessor.GetModel<TModel>();
+            OnGetModel(model, typeof(TModel));
+
+            return model;
         }
 
         public virtual TModel GetModel<TModel>(Func<Control, Type, Object> valueGetter) where TModel : class
         {
-            return ModelProcessor.GetModel<TModel>(valueGetter);
+            var model = ModelProcessor.GetModel<TModel>(valueGetter);
+            OnGetModel(model, typeof(TModel));
+
+            return model;
         }
 
         public virtual void FillModel<TModel>(TModel model) where TModel : class
         {
             ModelProcessor.FillModel(model);
+            OnFillModel(model, typeof(TModel));
         }
 
         public virtual void FillModel<TModel>(TModel model, Func<Control, Type, object> valueGetter) where TModel : class
         {
             ModelProcessor.FillModel(model, valueGetter);
+            OnFillModel(model, typeof(TModel));
         }
 
         public virtual void SetModel<TModel>(TModel model) where TModel : class
@@ -90,6 +106,14 @@ namespace ASP.NET.TwoWayModel.UIBases
         #endregion
 
         #region Overridables
+
+        protected virtual void OnGetModel(Object model, Type type)
+        {
+        }
+
+        protected virtual void OnFillModel(Object model, Type type)
+        {
+        }
 
         protected virtual void OnSetModel(Object model, Type type)
         {
