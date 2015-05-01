181e1dc3edd36e6cdb773476ec5d684fa0fd1dc2
Zhestokus <v.ezugbaia@gmail.com>
2015-05-01 14:11:25 +0400

    dotNet-ASP.NET.TwoWayModel

---
+++ ASP.NET.TwoWayModel/UIBases/Generic/MasterPageModelBase.cs
@@ -0,0 +1,23 @@
+namespace ASP.NET.TwoWayModel.UIBases.Generic
+{
+    public abstract class MasterPageModelBase<TModel> : MasterPageModelBase where TModel : class, new()
+    {
+        private TModel _model;
+        public TModel Model
+        {
+            get
+            {
+                _model = (_model ?? GetModel<TModel>());
+                return _model;
+            }
+            set
+            {
+                //if (ReferenceEquals(_model, value))
+                //    return;
+
+                _model = value;
+                SetModel(_model);
+            }
+        }
+    }
+}
---
+++ ASP.NET.TwoWayModel/UIBases/Generic/UserControlModelBase.cs
@@ -0,0 +1,23 @@
+﻿namespace ASP.NET.TwoWayModel.UIBases.Generic
+{
+    public abstract class UserControlModelBase<TModel> : UserControlModelBase where TModel : class, new()
+    {
+        private TModel _model;
+        public TModel Model
+        {
+            get
+            {
+                _model = (_model ?? GetModel<TModel>());
+                return _model;
+            }
+            set
+            {
+                //if (ReferenceEquals(_model, value))
+                //    return;
+
+                _model = value;
+                SetModel(_model);
+            }
+        }
+    }
+}
